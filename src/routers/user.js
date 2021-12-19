c6b91162cb9c81e640342503e9508c3aa3da2401
purushothaman-source <72643364+purushothaman-source@users.noreply.github.com>
2021-12-19 16:30:06 +0530

    handling express  errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -30,9 +30,16 @@ const upload = multer({
   }
 });
 
-router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
-  res.send();
-});
+router.post(
+  "/users/me/avatar",
+  upload.single("avatar"),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 router.post("/users/login", async (req, res) => {
   try {
